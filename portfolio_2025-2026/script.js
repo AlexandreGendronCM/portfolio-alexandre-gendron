13f12d504b66a5385aaf67d8ec120f05f8397777
AlexandreGendronCM <143230188+AlexandreGendronCM@users.noreply.github.com>
2025-10-10 14:28:11 -0400

    Les bandes bougent

---
+++ portfolio_2025-2026/script.js
@@ -0,0 +1,16 @@
+let bandeProjetTxtWidth = document.getElementById("bande_projet").getBoundingClientRect().width;
+console.log(bandeProjetTxtWidth*2);
+
+gsap.fromTo("#bande_projet",{x:0},{x:-(bandeProjetTxtWidth*2), duration: ((bandeProjetTxtWidth*2)/143), ease: "none", repeat: -1});
+
+let bandeAproposContactTxtWidth = document.getElementById("bande_apropos_contact").getBoundingClientRect().width;
+console.log(bandeAproposContactTxtWidth*2);
+
+gsap.fromTo("#bande_apropos_contact",{x:0},{x:-(bandeAproposContactTxtWidth*2), duration: ((bandeAproposContactTxtWidth*2)/143), ease: "none", repeat: -1});
+
+let bandeCompetencesTxtWidth = document.getElementById("bande_competences").getBoundingClientRect().width;
+console.log(bandeCompetencesTxtWidth*2);
+
+gsap.fromTo("#bande_competences",{x:0},{x:-(bandeCompetencesTxtWidth*2), duration: ((bandeCompetencesTxtWidth*2)/143), ease: "none", repeat: -1});
+
+
