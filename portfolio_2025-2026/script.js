bebb15e1d820cc177422b7879be58a9e51e8dac9
AlexandreGendronCM <143230188+AlexandreGendronCM@users.noreply.github.com>
2025-10-12 22:50:08 -0400

    ajout json + fetch projets

--- portfolio_2025-2026/script.js
+++ portfolio_2025-2026/script.js
@@ -1,18 +1,53 @@
 let bandeProjetTxtWidth = document.getElementById("bande_projet").getBoundingClientRect().width;
-console.log(bandeProjetTxtWidth*2);
+console.log(bandeProjetTxtWidth * 2);
 
-gsap.fromTo("#bande_projet",{x:0},{x:-(bandeProjetTxtWidth*2), duration: ((bandeProjetTxtWidth*2)/143), ease: "none", repeat: -1});
+gsap.fromTo("#bande_projet", { x: 0 }, { x: -(bandeProjetTxtWidth * 2), duration: ((bandeProjetTxtWidth * 2) / 143), ease: "none", repeat: -1 });
 
 let bandeAproposContactTxtWidth = document.getElementById("bande_apropos_contact").getBoundingClientRect().width;
-console.log(bandeAproposContactTxtWidth*2);
+console.log(bandeAproposContactTxtWidth * 2);
 
-gsap.fromTo("#bande_apropos_contact",{x:0},{x:-(bandeAproposContactTxtWidth*2), duration: ((bandeAproposContactTxtWidth*2)/143), ease: "none", repeat: -1});
+gsap.fromTo("#bande_apropos_contact", { x: 0 }, { x: -(bandeAproposContactTxtWidth * 2), duration: ((bandeAproposContactTxtWidth * 2) / 143), ease: "none", repeat: -1 });
 
 let bandeCompetencesTxtWidth = document.getElementById("bande_competences").getBoundingClientRect().width;
-console.log(bandeCompetencesTxtWidth*2);
+console.log(bandeCompetencesTxtWidth * 2);
 
-gsap.fromTo("#bande_competences",{x:0},{x:-(bandeCompetencesTxtWidth*2), duration: ((bandeCompetencesTxtWidth*2)/143), ease: "none", repeat: -1});
-
-
+gsap.fromTo("#bande_competences", { x: 0 }, { x: -(bandeCompetencesTxtWidth * 2), duration: ((bandeCompetencesTxtWidth * 2) / 143), ease: "none", repeat: -1 });
 
 
+const appli = Vue.createApp({
+
+    data() {
+        return {
+            tableau_projets: []
+        };
+    },
+    mounted() {
+
+
+        
+fetch("./projets.json") 
+  .then(data => data.json()) 
+  .then(projet => { 
+    this.tableau_projets = projet;
+    console.log(this.tableau_projets)
+    
+  });
+        
+
+
+
+
+    },
+    methods: {
+        // ...
+    }
+});
+
+const vm = appli.mount('#section_projets');
+
+
+
+
+console.log(this.tableau_projets);
+
+app.mount('#section_projet');
